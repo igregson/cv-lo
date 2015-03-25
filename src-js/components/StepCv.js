f299a50db842bf5a29e16a9d0a0e947684a37b9f
isaac <mulchkinn@gmail.com>
2015-03-25 22:06:37 +0300

    lodash's sortByAll to the rescue :)

--- src-js/components/StepCv.js
+++ src-js/components/StepCv.js
@@ -2,6 +2,8 @@
 var React = require('react')
 var BtnPrevStep = require('./BtnPrevStep')
 var BtnNextStep = require('./BtnNextStep')
+var parseJson = require('./../lib/parseJson')
+var _ = require('lodash')
 // var pdfMake = require('../vendor/pdfmake')
 // var createPdf = require('../vendor/vfs_fonts')
 
@@ -12,80 +14,120 @@ module.exports = React.createClass ({
     
     var data = this.props.data.body
 
+
     // KEYS
     var lists = []
     for (var i = 0; i < data.lists.length; i++) {
       var list = data.lists[i]
 
-      lists.push({
-        text: list.name, 
-        id: list.id,
-        style: 'sectionHeader'
-      })
+        console.log(list.name)
+        lists.push({
+          text: list.name, 
+          id: list.id,
+          style: 'sectionHeader'
+        })
     }
     console.dir(lists)
 
     // VALUES
-    for (var i = 0; i < data.cards.length; i++) {
-      var card = data.cards[i]
+    // for (var i = 0; i < data.cards.length; i++) {
+    //   var card = data.cards[i]
 
       // nested array to add each card 
       // to its parent list
       // TODO: consider a way to approach this 
       //       without a nested loop, if there
       //       is one
-      for (var j = 0; j < lists.length; j++) {
-        var list = lists[j]
-        if (card.idList === list.id) {
-          lists.splice(j + 1, 0, {
-            text: card.name,
-            style: 'subheader'
-          })
-          // if there's a description, add it to array
-          // just after the last one added, hence "j + **2**"
-          if ( card.desc ) {
-            lists.splice(j + 2, 0, {
-              text: card.desc,
-              style: 'paragraph'
-            }) 
-          }
-        }
-      } // end nested loop
-    } // end loop
-    // var cards = []
-    // for (var i = 0; i < data.cards.length; i++) {
-    //   var card = data.cards[i]
+      // for (var j = 0; j < lists.length; j++) {
+      //   var list = lists[j]
+      //   if (card.idList === list.id && card.labels) {
 
-    //   cards.push({
-    //     id: card.idList,
-    //     text: card.name,
-    //     style: 'subheader'
-    //   })
-    //   // if there's a description, add it 
-    //   if ( card.desc ) {
-    //     cards.push({
-    //       text: card.desc,
-    //       style: 'paragraph'
-    //     }) 
-    //   }
+      //   } else if (card.idList === list.id) {
+      //     lists.splice(j + 1, 0, {
+      //       text: card.name,
+      //       style: 'subheader'
+      //     })
+      //     // if there's a description, add it to array
+      //     // just after the last one added, hence "j + **2**"
+      //     if ( card.desc ) {
+      //       lists.splice(j + 2, 0, {
+      //         text: card.desc,
+      //         style: 'paragraph'
+      //       }) 
+      //     }
+      //   }
+      // } // end nested loop
     // } // end loop
-    // console.dir(cards)
+    var cards = []
+    for (var i = 0; i < data.cards.length; i++) {
+      var card = data.cards[i]
 
-    // console.dir(lists)
-    // var content = [
-    //   {
-    //     text: data.,
-    //     style: 'header'
-    //   }
-    // ]
+      cards.push({
+        id: card.idList,
+        text: card.name,
+        style: 'subheader'
+      })
+      // if there's a description, add it 
+      if ( card.desc ) {
+        cards.push({
+          id: card.idList,
+          text: card.desc,
+          style: 'paragraph'
+        }) 
+      }
+    } // end loop
+    console.dir(cards)
 
-    // content.splice(1, 0, lists)    
-    var content = lists
+
+    // var arrayCombined = lists.concat(cards).sort(function(a,b) {
+
+    //   return (a.id < b.id) ?  1 : 
+    //         ((a.id > b.id) ? -1 : 0);
+
+    //   // if (a.id < b.id) {
+    //   //   return 1
+    //   // } else if (a.id > b.id) {
+    //   //   return -1
+    //   // } else {
+    //   //   return 0
+    //   // }
+
+    // }).reverse()
+    var arrayConcated = lists.concat(cards)
+    var arrayCombined = _.sortByAll(arrayConcated, 'id')
+
+    // var result = [];
+    // // Atleast one of the arrays should have elements
+    // while(lists.length || cards.length) {
+    //     // If both of them have elements
+    //     if (lists.length && cards.length) {
+    //         // Compare the first elements's ids
+    //         if (lists[0].id <= cards[0].id) {
+    //             // If short's id is smaller then push short's element
+    //             result.push(lists.shift());
+    //         } else {
+    //             // otherwise long's element
+    //             result.push(cards.shift());
+    //         }
+    //     } else if (lists.length) {
+    //         // we have only short's elements left.
+    //         result.push(lists.shift());
+    //     } else {
+    //         // we have only long's elements left.
+    //         result.push(cards.shift());
+    //     }
+    // }
+
+    console.dir(arrayCombined)
+    // console.dir(result)
+
+    // remove first header (since it's just for bio/meta info)
+    // lists.shift()
+    var content = arrayCombined
+    // var content = result
 
     var docDefinition = {
-
       content: content,
-
       styles: {
         header: {
           fontSize: 22,
@@ -137,3 +179,4 @@ module.exports = React.createClass ({
     )
   }
 })
+
---
+++ src-js/lib/parseJson.js
@@ -0,0 +1,50 @@
+// http://techslides.com/how-to-parse-and-search-json-in-javascript
+
+//return an array of objects according to key, value, or key and value matching
+var getObjects = exports.getObjects = function(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));    
+        } else 
+        //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+        if (i == key && obj[i] == val || i == key && val == '') { //
+            objects.push(obj);
+        } else if (obj[i] == val && key == ''){
+            //only add if the object is not already in the array
+            if (objects.lastIndexOf(obj) == -1){
+                objects.push(obj);
+            }
+        }
+    }
+    return objects;
+}
+ 
+//return an array of values that match on a certain key
+var getValues = exports.getValues = function(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getValues(obj[i], key));
+        } else if (i == key) {
+            objects.push(obj[i]);
+        }
+    }
+    return objects;
+}
+ 
+//return an array of keys that match on a certain value
+var getKeys = exports.getKeys = function(obj, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getKeys(obj[i], val));
+        } else if (obj[i] == val) {
+            objects.push(i);
+        }
+    }
+    return objects;
+}
